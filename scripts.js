c4ec771f3662f8965fea27056d83126afa561452
Colin Wymore <colinwymore@gmail.com>
2017-04-18 14:29:15 -0500

    Final Commit

--- scripts.js
+++ scripts.js
@@ -3,22 +3,34 @@ var people = ['colin', 'charlie', 'jorge', 'ryan'];
 
 var rando = people[Math.floor(Math.random()*people.length)];
 
+// var pic = url(https://img.clipartfest.com/524c5a4d699f21f3d0ec1ec209c058d3_dark-green-check-mark-clip-art-green-check-mark-clipart_600-461.png);
 
 $(document).ready(function(){
 
 
   console.log('jQuery test');
-  $('#name').text(rando); //add arr to span
+
+
+  $('#name').text(rando); //add person to span
+
+
 
   $('.pic').on('click', function(){
 
     if( $(this).data('name') === $("#name").text()) {
 
-        alert('You Are Correct!!');
+        // pic.show( "drop", {direction: "down"}, 1000 );
+
+        $('body').css({'background-image': "url('check.png')"});
+          alert('You Are Correct!!');
+        setTimeout(location.reload.bind(location), 5000);
 
       } else {
 
+
+        $(this).effect( "explode", {times:4}, 5000 );
         alert('You Are Wronger!!');
+
 
       }
 
@@ -26,12 +38,6 @@ $(document).ready(function(){
 
 
 });
-
-
-
-
-
-
 
 
 
